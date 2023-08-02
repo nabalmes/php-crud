4c7cb310849dda340db77341ea6d7ae9ac5e1a24
nabalmes <neilbalmes4200@gmail.com>
2023-08-02 22:47:52 +0800

    added latest

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php 
+    class Database {
+        private $host = "localhost";
+        private $database_name = "phpapidb";
+        private $username = "root";
+        private $password = "password";
+        public $conn;
+       
+    }  
+?>
