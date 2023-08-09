ef7d931909e3d0b5882037d77227b51d850a5ab8
nabalmes <neilbalmes4200@gmail.com>
2023-08-09 16:29:46 +0800

    fix delete functionality

--- index.php
+++ index.php
@@ -8,10 +8,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css">
 </head>
+<style>
+    .header-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 100%;
+    }
+</style>
 
 <body>
     <div class="container mt-5">
-        <h2>Employee Data</h2>
+        <div class="header-container">
+            <h2>Employee Data</h2>
+            <buttontype="button" class="btn btn-primary" data-toggle="modal" data-target="#addModal">Add Employee</button>
+        </div>
         <table class="table table-bordered">
             <thead>
                 <tr>
@@ -27,6 +38,44 @@
 
             </tbody>
         </table>
+    </div>
+    <!-- add Modal -->
+    <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form method="post">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" name="name">
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="age" class="form-label">Age</label>
+                            <input type="number" class="form-control" name="age" id="age">
+                        </div>
+                        <div class="mb-3">
+                            <label for="designation" class="form-label">Designation</label>
+                            <input type="text" class="form-control" name="designation" id="designation">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button id="btnSubmitAE" type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Edit Modal -->
@@ -108,7 +157,7 @@
                     <td>${user.created}</td>
                     <td>
                         <button class="btn btn-primary btn-sm edit-btn editBtn" data-toggle="modal" data-target="#editModal">Edit</button>
-                        <button id="deleteButton" class="btn btn-danger btn-sm delete-btn">Delete</button>
+                        <button id="deleteButton" class="btn btn-danger btn-sm delete-btn" data-id="${user.id}">Delete</button>
                     </td>
                 </tr>
                 `;
@@ -146,6 +195,65 @@
         // Fetch the JSON data when the page loads
         fetchUserData();
         $(document).ready(function() {
+            //Posting data
+            $('#btnSubmitAE').click(function() {
+                var name = $('#name').val();
+                var email = $.trim($('#email').val());
+                var age = $.trim($('#age').val());
+                var designation = $.trim($('#designation').val());
+
+                var requestData = JSON.stringify({
+                    'name': name,
+                    'email': email,
+                    'age': age,
+                    'designation': designation,
+                });
+                console.log('Data being sent:', requestData);
+                // console.log(name, email, age, designation);
+                if (name == "" || email == "" || age == "" || designation == "") {
+                    Swal.fire({
+                        text: "Fill out required fields.",
+                        icon: "error",
+                        buttonsStyling: false,
+                        confirmButtonText: "OK",
+                        customClass: {
+                            confirmButton: "btn font-weight-bold btn-light",
+                        },
+                    });
+                    return;
+                } else {
+                    $.ajax({
+                        url: "api/create.php",
+                        method: "POST",
+                        data: JSON.stringify({
+                            'name': name,
+                            'email': email,
+                            'age': age,
+                            'designation': designation,
+
+                        }),
+                        success: function() {
+                            Swal.fire({
+                                    text: "User added successfully.",
+                                    icon: "success",
+                                    buttonsStyling: false,
+                                    confirmButtonText: "OK",
+                                    customClass: {
+                                        confirmButton: "btn font-weight-bold btn-light",
+                                    },
+                                })
+                                .then((result) => {
+                                    if (result.isConfirmed) {
+                                        location.reload();
+                                    }
+                                });
+
+                        }
+                    });
+                }
+            });
+
+            //edit btn
             $(document).on("click", ".editBtn", function() {
                 $tr = $(this).closest('tr');
 
@@ -161,8 +269,10 @@
                 $('#editAge').val(data[3])
                 $('#editDesignation').val(data[4])
                 $('#editCreated').val(data[5])
-            });
 
+                const deleteID = data[0];
+            });
+            //save button
             $(document).on("click", "#saveChanges", function() {
                 const id = $('#updateID').val();
                 const name = $('#editName').val();
@@ -221,6 +331,38 @@
                 });
 
             });
+
+            $(document).on("click", "#deleteButton", function() {
+                var itemId = $(this).data("id");
+                var deleteID = {
+                    "id": itemId
+                }
+                console.log("deleteID:", deleteID);
+
+                Swal.fire({
+                    html: `Are you sure you want to delete?<br><br>`,
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#d33",
+                    cancelButtonColor: "#3085d6",
+                    confirmButtonText: "Yes, Delete it!",
+                    cancelButtonText: "Cancel"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // alert(JSON.stringify(updatedData))
+                        // console.log("updated:", updatedData);
+                        $.ajax({
+                            url: 'api/delete.php',
+                            method: 'DELETE',
+                            dataType: 'json',
+                            data: JSON.stringify(deleteID),
+                            success: function(response) {
+                                location.reload();
+                            }
+                        })
+                    }
+                });
+            })
         });
     </script>
 </body>
